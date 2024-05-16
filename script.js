7588cc52ff2f42ebbb92a803cbdd2098e089bdab
JoseLopes67 <josepedrogomes.lopes67@gmail.com>
2024-05-16 12:40:29 +0100

    earth

---
+++ script.js
@@ -0,0 +1,16 @@
+import * as THREE from 'three';
+
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+
+const scene = new THREE.Scene();
+
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+let object;
+
+let controls;
+
+let objToRender = 'world';
+
+const loader = new GLTFLoader();
